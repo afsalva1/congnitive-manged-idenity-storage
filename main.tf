2ec2fa6122963c8cb5ac76e1aebecca4981cf804
afsalva1 <42220161+afsalva1@users.noreply.github.com>
2021-07-20 21:33:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "storage_RG" {
 }
 
 resource "azurerm_storage_account" "afsal_stg_account" {
-  name                     = "afsalstorageaccountfivetoone"
+  name                     = "afsalstotfivetoone"
   resource_group_name      = azurerm_resource_group.storage_RG.name
   location                 = azurerm_resource_group.storage_RG.location
   account_tier             = "Standard"
