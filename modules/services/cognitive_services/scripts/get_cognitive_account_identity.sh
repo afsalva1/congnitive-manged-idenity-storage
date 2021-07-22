1837f54271c64813f44249672c0e9396798b0281
afsalva1 <42220161+afsalva1@users.noreply.github.com>
2021-07-22 19:40:03 +0530

    Login CLI

--- modules/services/cognitive_services/scripts/get_cognitive_account_identity.sh
+++ modules/services/cognitive_services/scripts/get_cognitive_account_identity.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 cognitive_account_name="$1"
 cognitive_account_resource_group="$2"
 
+loginAzureCLI = "$(az login --service-principal -u 4242bfb1-0024-4bdf-a684-56df78790c9a -p _94f-.6nC05uQK_P894PA-RRl22-QS79wM --tenant 4c4aede3-daf6-4c76-8ec8-879c2531a537)"
 identityPrincipalId="$(az cognitiveservices account identity show --name $cognitive_account_name --resource-group $cognitive_account_resource_group --query principalId --output tsv || true)"
 
 echo '{ "identityPrincipalId": "'$identityPrincipalId'" }'
