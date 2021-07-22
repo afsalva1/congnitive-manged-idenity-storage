10b63b3ef54fd07645efb7793da35311f793fbb4
afsalva1 <42220161+afsalva1@users.noreply.github.com>
2021-07-22 19:15:06 +0530

    Update main.tf

--- modules/services/cognitive_services/main.tf
+++ modules/services/cognitive_services/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_cognitive_account" "main" {
-  name                  = "afsaL-cogntive-cogsvc-test-qwertyuiopfive"
+  name                  = "afsaL-cogntive-cogsvc-test-qwertyuiopeight"
   location              = var.location
   resource_group_name   = var.resource_group_name
   kind                  = var.kind
